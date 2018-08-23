60f18bd93e1d2843f5e6e1e875392725e43e40d6
mupengling123 <1012968673@qq.com>
2018-08-23 16:10:43 +0800

    这是需要传送的文件

---
+++ scripts/test_login1.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_login11(self):
+        assert 1
+
+    def test_login21(self):
+        assert 0
+
+    def test_login31(self):
+        assert 1
+
+    def test_login41(self):
+        assert 1
